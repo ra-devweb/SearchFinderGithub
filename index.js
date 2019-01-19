f5f7135dfad887f7d98cef4e9acf813f7968156f
Tarik Razine <ra.devweb.io@gmail.com>
2019-01-19 14:18:13 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,59 +6,58 @@ const getIdAndSecretKeys    =  async () => {
 
     const resIdSecret   =  await response.json();
     
-    return {resIdSecret.id, resIdSecret.secret}
+    // Init class Github
+
+    const github    =  new Github(resIdSecret.id, resIdSecret.secret);
+
+    console.log(github);
+
+    // Init profile UI
+
+    const ui        =  new UI();
+
+    // Search input
+
+    const searchUser    =   document.getElementById('searchUser');
+
+    // Search input event listener
+
+    searchUser.addEventListener('keyup', (e) => {
+
+        const userText  =   e.target.value;
+
+        if (userText !== '') {
+
+            // Make http call
+
+            github.getUser(userText)
+                .then( data => { 
+                    if(data.profile.message === 'Not Found') {
+
+                        // Show alert
+                        ui.showAlert(':( User Not Found', 'message is-danger');
+
+                    } else {
+
+                        // Show profile
+                        ui.showProfile(data.profile);
+
+                        // Show repos
+                        ui.showRepos(data.repos);
+
+                    }
+                })
+
+        } else {
+
+            // Clear profile
+
+            ui.clearProfile();
+
+        }
+
+    });
 
 }
 
-// Init class Github
-
-const github    =  new Github(getIdAndSecretKeys());
-
-console.log(github);
-
-// Init profile UI
-
-const ui        =  new UI();
-
-// Search input
-
-const searchUser    =   document.getElementById('searchUser');
-
-// Search input event listener
-
-searchUser.addEventListener('keyup', (e) => {
-
-    const userText  =   e.target.value;
-
-    if (userText !== '') {
-
-        // Make http call
-        
-        github.getUser(userText)
-            .then( data => { 
-                if(data.profile.message === 'Not Found') {
-
-                    // Show alert
-                    ui.showAlert(':( User Not Found', 'message is-danger');
-
-                } else {
-
-                    // Show profile
-                    ui.showProfile(data.profile);
-
-                    // Show repos
-                    ui.showRepos(data.repos);
-
-                }
-            })
-
-    } else {
-
-        // Clear profile
-
-        ui.clearProfile();
-
-    }
-
-});
-
+getIdAndSecretKeys()
