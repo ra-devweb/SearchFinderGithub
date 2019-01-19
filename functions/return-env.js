8950e51cb9669f45c18ae8e6a3d53c46eddac5b7
Razine Tarik <howmegacom@gmail.com>
2019-01-19 11:49:31 +0100

    add variables lambda

--- functions/return-env.js
+++ functions/return-env.js
@@ -3,7 +3,7 @@ exports.handler = function(event, context, callback) {
     // Using ES6 destructuring to pull the variable called FAKE_TOKEN out of the process.env object
     // You can then use the FAKE_TOKEN anywhere in your code just as you would any string variable
     const { ID, SECRET } = process.env;
-    console.log("Work", "work");
+    console.log(ID, SECRET);
     // Amazon callback function without any custom headers
     callback(null, {
       statusCode: 200,
