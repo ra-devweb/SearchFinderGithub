17824645c3388b8e4c332db03d4273132d40a5a7
Tarik Razine <ra.devweb.io@gmail.com>
2019-01-19 13:38:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,27 +12,20 @@ const getIdAndSecretKeys    =  async (url) => {
 
 const url   =   '/.netlify/functions/return-env';
 
-getIdAndSecretKeys(url)
-    .then(data => {
 
-        const id        = data.id;
-
-        const secret    =   data.secret;
-
-        return new Github(id, secret);
-    
-    });
-
-/* 
-***
-*** If you want to entre manualy your keys please comment getIdAndSecretKeys function
-*** and enter the id key and secret to new Github(id, secret) 
-***
-*/
 
 // Init class Github
 
-const github    =   getIdAndSecretKeys();
+const github    =   getIdAndSecretKeys(url)
+                        .then(data => {
+
+                            const id        = data.id;
+
+                            const secret    =   data.secret;
+
+                            return new Github(id, secret);
+
+                        });
 
 console.log(github);
 
